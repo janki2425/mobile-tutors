a199bee584e8ee6734b8e6f381b12df2c6b328ae
janki2425 <152633106+janki2425@users.noreply.github.com>
2025-05-08 13:07:01 +0530

    benefit section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import Video from "@/components/Video";
 import About from "@/components/About";
+import Benefits from "@/components/Benefits";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Video />
       <About />
+      <Benefits />
     </div>
   );
 }
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,17 +3,17 @@ import React from 'react'
 const About = () => {
   return (
     <div className='w-full max-w-[1360px] mx-auto my-[80px]'>
-      <div className='flex flex-col md:flex-row gap-[120px] items-center md:itmes-start'>
-        <div>
-            <h6 className='H6-20 font-[600]'>About Us</h6>
+      <div className='flex flex-col xl:flex-row gap-[40px] xl:gap-[110px] justify-content-around xl:itmes-start'>
+        <div className='mx-auto xl:mx-0'>
+            <h6 className='H6-20 font-[600]'>ABOUT US</h6>
         </div>
         <div className='flex flex-col gap-[40px]'>
-            <h3 className='text-left mx-auto md:H3-48'>
+            <h3 className='text-center max-w-[800px] xl:text-start mx-auto xl:mx-0 px-4 xl:px-0 font-[500] H3-48'>
             Hi! We are Mobile Tutors
             The most effective, engaging, and affordable online learning platform
             </h3>
-            <p className='P-16 font-[500]'>John Stephen, CEO</p>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[73px]'>
+            <p className='P-16 font-[500] text-center xl:text-left'>John Stephen, CEO</p>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 lg:px-4 gap-[73px] mx-auto xl:mx-0'>
                 <div className='max-w-[230px] flex flex-col gap-[16px] bg-gold rounded-[24px] p-[24px] items-start justify-center'>
                     <h2 className='text-[74px] font-[500] leading-[120%]'>70+</h2>
                     <div className='flex flex-col gap-[8px]'>
