cb222d58d741bee975e356ec6ca838384402a0bf
janki2425 <152633106+janki2425@users.noreply.github.com>
2025-05-15 15:30:20 +0530

    fixed bugs

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -5,39 +5,12 @@ import { Button } from '@/components/ui/button'
 import SuccessStory from '@/components/SuccessStory'
 import Contact from '@/components/Contact'
 import { useRouter } from 'next/navigation'
+import { blogData } from '@/utils/data'
 
 const blogCategory = [
     'TIPS FOR EXAM SUCCESS',
     'SUBJECTS INSIGHTS',
     'OTHERS',
-]
-
-export const blogData = [
-        {
-            id: 1,
-            title: 'Personalised Assessment',
-            description: "We assess your child's strengths, weaknesses, and learning style to create a customised plan that targets their unique needs and ensures clear progress.",
-            image: '/blogs/blogs-img.jpg',
-        },
-        {
-            id: 2,
-            title: 'Personalised Assessment',
-            description: "We assess your child's strengths, weaknesses, and learning style to create a customised plan that targets their unique needs and ensures clear progress.",
-            image: '/blogs/blogs-img.jpg',
-        },
-        {
-            id: 3,
-            title: 'Personalised Assessment',
-            description: "We assess your child's strengths, weaknesses, and learning style to create a customised plan that targets their unique needs and ensures clear progress.",
-            image: '/blogs/blogs-img.jpg',
-        },
-        {
-            id: 4,
-            title: 'Personalised Assessment',
-            description: "We assess your child's strengths, weaknesses, and learning style to create a customised plan that targets their unique needs and ensures clear progress.",
-            image: '/blogs/blogs-img.jpg',
-        },
-
 ]
 
 const Blogs = () => {
