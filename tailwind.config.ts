4b85a43c4aced88fcd7ba35615e343114b42fc50
janki2425 <152633106+janki2425@users.noreply.github.com>
2025-05-26 14:57:23 +0530

    fixed sidebar issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,27 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './src/app/**/*.{js,ts,jsx,tsx}',
+//     './src/components/**/*.{js,ts,jsx,tsx}',
+//   ],
+//   theme: {
+//     extend: {
+      
+//     },
+//   },
+//   plugins: [
+    
+//   ],
+// };
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
